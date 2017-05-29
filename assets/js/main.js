71eb66a2f223ed74ee0bbb05e074b307966d5ea8
uriahgray <uriahgray@MacBook-Air-2.local>
2017-05-29 19:09:51 +1000

    push site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
-  $('.circle').click(function() {
-    $('img').addClass('showme');
+
+
+// Add jQuery here
+
+
+
+$('.morelink').click(function(event) {
+  $('.more').toggle();
+});
+
+
+
+$('.text').each(function(index, el) {
+  if ($(this).prop('scrollWidth') > $(this).parent().width() ) {
+    $(this).addClass('overflowing');
+  }
+  else {
+   $(this).addClass('noverflow');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
   });
-})
