09c473b922e612e832d97ac98ac72b961966ba4a
uriahgray <uriahgray@MacBook-Air-2.local>
2017-06-04 13:51:43 +1000

    push random

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,32 @@ $('.text').each(function(index, el) {
 });
 
 
+(function($) {
 
+$.fn.randomize = function(childElem) {
+  return this.each(function() {
+      var $this = $(this);
+      var elems = $this.children(childElem);
+
+      elems.sort(function() { return (Math.round(Math.random())-0.5); });
+
+      $this.remove(childElem);
+
+      for(var i=0; i < elems.length; i++)
+        $this.append(elems[i]);
+
+  });
+}
+})(jQuery);
+
+$(".col1 a").css('opacity', '0');
+
+
+setTimeout(function(){
+  $(".col1 a").css('opacity', '1');
+  $(".col1").randomize("a");
+
+}, 500);
 
 
 
